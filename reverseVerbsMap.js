e43174325ef21ffe7495b32ea1f68e585cb92c5e
ianhunterharold <ianhunterharold@gmail.com>
2020-07-02 08:34:43 -0600

    working solution with maps

--- reverseVerbsMap.js
+++ reverseVerbsMap.js
@@ -2,53 +2,23 @@ var reverseVowels = function(s) {
     
   let vowelTank = [];
   let splitBy = s.split('');
-  let vowels = ["a","e","i","o","u"];
-
+  let vowels = ["a","A","e","E","i","I","o","O","u","U"];
+    
     const replacedVowels = splitBy.map(char => {
       if ( vowels.includes(char) ){
         let addToVowelTank = vowelTank.unshift(char);
-        return "0"
+        return "#/#"
       } else {
         return char;
       }
     });
-
-    // [wip]
-  return const reverseVowels = splitBy.map(chars => {
-    let k =0;
-    if (chars ==="0"){
-      console.log(chars,"did it happen")
-      return char = vowelTank[k]
-      k++
+  
+  const swappedVowel = replacedVowels.map(char => {
+    if (char ==="#/#"){
+      return vowelTank.shift();
     } else {
       return char;
     }
-  })
-
-  
-
-
-  // for(let i=0; i <splitBy.length; i++){
-  //     if(splitBy[i] === 'a' || splitBy[i] === 'e' || splitBy[i] === 'i'|| splitBy[i] === 'o'|| splitBy[i] === 'u'){
-          
-  //         let addToVowelTank = vowelTank.push(splitBy[i];
-  //         splitBy[i] = "0"; //replace vowel with "0"
-  //     };
-  // };
-
-//   let reverseVowelTank = vowelTank.reverse();
-
-
-//   for (let j =0, k=0; j < splitBy.length, k < reverseVowelTank.length; j++){
-//     if (splitBy[j] === '0'){
-//        splitBy[j] = reverseVowelTank[k];
-//        k++
-//     }
- 
-//   } 
-// return splitBy.join('');
+  });
+    return swappedVowel.join('');
 };
-
-
-const st='pizzahut';
-reverseVowels(st);
